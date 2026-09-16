0c154f3c73ceab030226746d4533adb5d6187c3f
tatsuki watanabe <story_2014@yahoo.co.jp>
2026-09-17 01:49:09 +0900

    update images and styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -143,3 +143,14 @@ tabs.forEach(tab => {
     document.getElementById(`tab-${target}`).classList.add('active');
   });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const navLinks = document.querySelectorAll("p-main__nav");
+  const currentURL = location.pathname;
+
+  navLinks.forEach(link => {
+    if (link.getAttribute("href") === currentURL) {
+      link.classList.add("active");
+    }
+  });
+});
